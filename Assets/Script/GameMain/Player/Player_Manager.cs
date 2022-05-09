fc74b2816fc1663e5f9b0953017a53a5882e7e78
yet <1607388033@qq.com>
2022-05-09 10:28:59 +0800

    更新迷你地图

--- Assets/Script/GameMain/Player/Player_Manager.cs
+++ Assets/Script/GameMain/Player/Player_Manager.cs
@@ -7,15 +7,13 @@ public class Player_Manager : SingletonMono<Player_Manager>
 {
     private Animator player_Animator;
     private Rigidbody2D player_Rigidbody2D;
-    private Transform player_Transform;//有刚体的物体才能移动
 
 
     protected override void Init_Awake()
     {
         base.Init_Awake();
         player_Animator = transform.Find_Child<Animator>(Config_Player.player_Prefab_Body);
-        player_Rigidbody2D = transform.Find_Child<Rigidbody2D>(Config_Player.player_Prefab_Body);
-        player_Transform = transform.Find_Child_Transform(Config_Player.player_Prefab_Body);
+        player_Rigidbody2D = transform.GetComponent<Rigidbody2D>();
     }
 
     public Animator Player_Animator
@@ -28,6 +26,6 @@ public class Player_Manager : SingletonMono<Player_Manager>
     }
     public Transform Player_Position
     {
-        get { return player_Transform; }
+        get { return transform; }
     }
 }
